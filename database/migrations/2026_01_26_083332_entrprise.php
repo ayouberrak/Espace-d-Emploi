235073d243749c9eb9a695779d0c216d45b9fa10
ayouberrak <ayouberrak30@gmail.com>
2026-01-26 16:21:19 +0100

    add migration for company

---
+++ database/migrations/2026_01_26_083332_entrprise.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entrprise' , function(Blueprint $table){
+            $table->id();
+            $table->string('nom');
+            $table->string('logo')->nullable();
+            $table->text('description');
+            $table->string('location');
+            $table->timestamp('create');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entrprise');
+    }
+};
