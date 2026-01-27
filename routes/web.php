d064fc5ca0b54518b538c920f1c4cf13ff9471e5
ayouberrak <ayouberrak30@gmail.com>
2026-01-27 20:43:21 +0100

    feat(routes): update application routes

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,26 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RecruiterController;
 
 // Main Dashboard
 Route::get('/', [UserController::class, 'index'])->name('dashboard');
 
 // New Platform Sections
-Route::get('/stages', [UserController::class, 'stages'])->name('stages');
+Route::get('/offres', [UserController::class, 'offres'])->name('offres');
 Route::get('/recruteurs', [UserController::class, 'recrutement'])->name('recrutement');
 
 // Authentication
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 
+Route::post('/login' , [AuthController::class , 'login'])->name('loginPost');
 Route::post('/register' ,[AuthController::class , 'store'])->name('registerPost');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Profile
-Route::get('/profile/{id?}', [UserController::class, 'profile'])->name('profile');
+Route::get('/profile/{id?}', [ProfileController::class, 'profile'])->name('profile');
 
 // Espace Recruteur
 Route::get('/recruiter/dashboard', [RecruiterController::class, 'dashboard'])->name('recruiter.dashboard');
