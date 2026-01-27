1e1eeca5328936421642ed28d45bdf40d15b7bf5
ayouberrak <ayouberrak30@gmail.com>
2026-01-27 23:02:13 +0100

    feat(user): update user controller functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Offres;
 
 class UserController extends Controller 
 {
@@ -19,7 +20,7 @@ class UserController extends Controller
                 'name' => $user->name,
                 'role' => $user->role,
                 'avatar' =>$user->photo ,
-                'specialty' => $user->profile?->title,
+                'specialty' => $user->profile?->title ,
                 'bio' => $user->profile?->bio ?? $user->bio ,
                 'skills' => $user->profile?->skills ,
             ];
@@ -28,18 +29,5 @@ class UserController extends Controller
         return view('users.index', compact('users'));
     }
 
-    public function offres()
-    {
-        
-        return view('pages.stages', compact('stages'));
-    }
-
-    public function recrutement()
-    {
-        $recruiters = array_filter($this->mockUsers, function($user) {
-            return $user['role'] === 'recruiter';
-        });
-        return view('pages.recrutement', compact('recruiters'));
-    }
 
 }
