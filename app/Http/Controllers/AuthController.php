463ec21bce4519b211235ac9f2b9a638092a325c
ayouberrak <ayouberrak30@gmail.com>
2026-01-27 12:19:10 +0100

    Update AuthController logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,19 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
+
+    public function store(Request $method){
+            $method->validate([
+                'first_name' =>'required|string|max:255',
+                'last_name' =>'required|string|max:255',
+                'email' =>'required|email|unique:users,email',
+                'password' =>'required|confirmed|min:8',
+                'role' =>'required|in:devloppeur,recruiter',
+            ]);
+
+            
+    }
+
     public function showRegister()
     {
         return view('auth.register');
