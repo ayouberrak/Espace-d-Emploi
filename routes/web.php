b93771cb5e2a3c8d1bfed80d781e4b8982be4820
ayouberrak <ayouberrak30@gmail.com>
2026-01-27 23:02:14 +0100

    feat(routes): update application routes

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,14 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RecruiterController;
+use App\Http\Controllers\recruteuController;
+use App\Http\Controllers\offresController;
 
-// Main Dashboard
 Route::get('/', [UserController::class, 'index'])->name('dashboard');
 
-// New Platform Sections
-Route::get('/offres', [UserController::class, 'offres'])->name('offres');
-Route::get('/recruteurs', [UserController::class, 'recrutement'])->name('recrutement');
+Route::get('/offres', [offresController::class, 'offres'])->name('offres');
+Route::get('/recruteurs', [recruteuController::class, 'recrutement'])->name('recrutement');
 
-// Authentication
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 
@@ -21,14 +20,11 @@ Route::post('/login' , [AuthController::class , 'login'])->name('loginPost');
 Route::post('/register' ,[AuthController::class , 'store'])->name('registerPost');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Profile
 Route::get('/profile/{id?}', [ProfileController::class, 'profile'])->name('profile');
 
-// Espace Recruteur
 Route::get('/recruiter/dashboard', [RecruiterController::class, 'dashboard'])->name('recruiter.dashboard');
 Route::get('/recruiter/jobs/create', [RecruiterController::class, 'createJob'])->name('recruiter.jobs.create');
 Route::post('/recruiter/jobs/store', [RecruiterController::class, 'storeJob'])->name('recruiter.jobs.store');
 
-// Social & Chat
 Route::get('/chat', [App\Http\Controllers\ChatController::class, 'index'])->name('chat.index');
 Route::get('/network', [App\Http\Controllers\NetworkController::class, 'index'])->name('network.index');
