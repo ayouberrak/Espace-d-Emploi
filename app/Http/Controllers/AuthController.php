14890c563f05e6de8449898fa3bda28c4fc92906
ayouberrak <ayouberrak30@gmail.com>
2026-01-27 20:43:19 +0100

    feat(auth): improve authentication logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
-class AuthController extends Controller
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Recruteur;
+use App\Models\Developpeur;
+
+class AuthController extends Controller 
 {
     public function showLogin()
     {
         return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            
+            $user = Auth::user();
+
+            session([
+                'role' => $user->role,
+                'user_id' => $user->id,
+            ]);
+
+
+
+            if ($user->role === 'recruiter') {
+                return redirect()->route('recruiter.dashboard');
+            }
+
+            return redirect()->route('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Les identifiants fournis ne correspondent pas à nos enregistrements.',
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login');
     }
 
 
@@ -21,7 +66,25 @@ class AuthController extends Controller
                 'role' =>'required|in:devloppeur,recruiter',
             ]);
 
-            
+
+            if($method->input('role') == 'recruiter'){
+                $user = Recruteur::create([
+                    'name' => $method->input('first_name').' '.$method->input('last_name'),
+                    'email' =>$method->input('email'),
+                    'password'=>Hash::make($method->input('password')),
+                    'role'=> 'recruiter'
+                ]);
+            }else{
+                $user = Developpeur::create([
+                    'name' => $method->input('first_name').' '.$method->input('last_name'),
+                    'email' =>$method->input('email'),
+                    'password'=>Hash::make($method->input('password')),
+                    'role'=> 'devloppeur'
+                ]);
+            }
+
+            Auth::login($user);
+            return redirect()->route('dashboard');
     }
 
     public function showRegister()
