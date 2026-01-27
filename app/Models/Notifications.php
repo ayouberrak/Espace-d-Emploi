bbf9ec32b8448160c81d1c631d7d64a6881b84bb
ayouberrak <ayouberrak30@gmail.com>
2026-01-27 12:19:17 +0100

    Add Notifications model

---
+++ app/Models/Notifications.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Notifications extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'type',
+        'data',
+        'read_at',
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+        'read_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
