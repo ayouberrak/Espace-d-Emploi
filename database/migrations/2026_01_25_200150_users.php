d43f467901e9234b668f64830aefebd73053fee0
ayouberrak <ayouberrak30@gmail.com>
2026-02-01 22:44:51 +0100

    Modify users table migration

--- database/migrations/2026_01_25_200150_users.php
+++ database/migrations/2026_01_25_200150_users.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('password');
             $table->enum('role',['devloppeur','recruiter']);
             $table->string('photo')->nullable();
+            $table->string('cover_image')->nullable();
             $table->text('bio')->nullable();
             $table->string('phone')->nullable();
             $table->jsonb('amis')->nullable();
