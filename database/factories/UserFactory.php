fe3b4b05ca105a59b55752c64e8f14c67da0c071
ayouberrak <ayouberrak30@gmail.com>
2026-02-01 15:34:29 +0100

    Update UserFactory with new attributes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,9 +26,12 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'role' => fake()->randomElement(['devloppeur', 'recruiter']),
+            'photo' => fake()->imageUrl(),
+            'bio' => fake()->paragraph(),
+            'phone' => fake()->phoneNumber(),
+            'amis' => [], 
         ];
     }
 
@@ -39,6 +42,20 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function recruiter(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'recruiter',
+        ]);
+    }
+
+    public function developer(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'devloppeur',
         ]);
     }
 }
