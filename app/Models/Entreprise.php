64ced2d0fe7bdc6943a89f7876ac72f6576db1d9
ayouberrak <ayouberrak30@gmail.com>
2026-02-01 15:34:29 +0100

    Update Entreprise model relationships and attributes

--- app/Models/Entreprise.php
+++ app/Models/Entreprise.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Entreprise extends Model
 {
+    use HasFactory;
     protected $table = 'entrprise';
 
     protected $fillable = [
