6b75ebb5e44dc0c9db17133eb6dae7eff0c78943
ayouberrak <ayouberrak30@gmail.com>
2026-02-03 16:21:10 +0100

    Configure application services and bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Broadcast;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Broadcast::routes();
+        require base_path('routes/channels.php');
+
+        \Illuminate\Support\Facades\View::composer('*', function ($view) {
+            if (auth()->check()) {
+                $notifications = auth()->user()->notifications()->whereNull('read_at')->latest()->take(5)->get();
+                $unreadCount = auth()->user()->notifications()->whereNull('read_at')->count();
+                $view->with('notifications', $notifications)->with('unreadCount', $unreadCount);
+            }
+        });
     }
 }
