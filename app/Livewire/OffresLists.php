66119bb134189ab36e5f66284fd5b2ec6534f8eb
ayouberrak <ayouberrak30@gmail.com>
2026-02-05 21:44:12 +0100

    Add Livewire components for dynamic UI

---
+++ app/Livewire/OffresLists.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+#[Lazy]
+class OffresLists extends Component
+{
+    public function render()
+    {
+        return view('livewire.offres-lists');
+    }
+}
