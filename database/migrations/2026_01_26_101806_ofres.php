a3d7a878f354f626e78f4f98dc988149a873f415
ayouberrak <ayouberrak30@gmail.com>
2026-02-06 16:55:13 +0100

    projet Done :)

--- database/migrations/2026_01_26_101806_ofres.php
+++ database/migrations/2026_01_26_101806_ofres.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ofres' , function (Blueprint $table){
+        Schema::create('    ' , function (Blueprint $table){
             $table->id();
             $table->foreignId('enptrise_id')
                     ->constrained('entrprise')
